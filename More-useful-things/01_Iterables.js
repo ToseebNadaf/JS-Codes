3f5e52fc893af0762aedf9aac9ef9965224d4b23
ToseebNadaf <toseebnadaf@gmail.com>
2025-01-14 10:49:47 +0530

    More useful things

---
+++ More-useful-things/01_Iterables.js
@@ -0,0 +1,32 @@
+// Iterables
+// jipar hum FOR-OF loop laga sake
+// string and arrays are iterables
+
+//string
+const firstName = "tosib";
+
+for (let n of firstName) {
+  console.log(n);
+}
+
+//OP- t -o -s -i -b
+//-------------------------------------
+
+// Array
+
+const array = ["item1", "item2", "item3"];
+
+for (let a of array) {
+  console.log(a);
+}
+//OP -item1 -item2 -item3
+//-------------------------------------
+
+// array like object
+// jinke pas length property hoti hai
+// aur jiko hum INDEX se access kar sakte hai
+// example :- string
+
+const midName = "dhrub";
+console.log(midName.length); //5 -- length property applicabel
+console.log(midName[3]); // u  -- accessing with index
